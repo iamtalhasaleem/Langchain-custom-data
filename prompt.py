70606d6689f1f2bfa0e3ff0443fe2386e0720c96
iamtalhasaleem <iamtalhasaleem911@gmail.com>
2024-09-13 12:44:21 +0500

    Embed User query in prompt.py

---
+++ prompt.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
+# from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
+# import langchain
+from langchain.chains import RetrievalQA
+
+# langchain.debug = True
+
+load_dotenv()
+embeddings = OpenAIEmbeddings()
+
+chat = ChatOpenAI()
+
+db = Chroma(
+    persist_directory="emb",
+    embedding_function=embeddings
+)
+
+retriever = db.as_retriever()
+
+chain = RetrievalQA.from_chain_type(
+    llm = chat,
+    retriever = retriever,
+    chain_type = "stuff"
+    # chain_type = "map_reduce"
+    # chain_type = "map_rerank"
+    # chain_type = "refine"
+)
+
+result = chain.run("What is an interesting fact about the English language ?")
+
+print("\n\n")
+print(result)
